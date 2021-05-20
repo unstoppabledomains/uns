25e085c37bb4827a0ba27676d2c16f09441c26e2
Sergii Bomko <aquila.ua@gmail.com>
2021-05-20 14:15:19 +0300

    merge Registry.SLDMinter tests with Registry tests

--- test/Registry.js
+++ test/Registry.js
@@ -188,7 +188,53 @@ describe('Registry', () => {
     })
   });
 
-  describe('Resolver', () => {
+  describe('Registry (SLD minter)', () => {
+    it('minting SLDs', async () => {
+      await registry.mintSLD(coinbase, 'label_22');
+  
+      const tok = await registry.childIdOf(await registry.root(), 'label_22');
+      assert.equal(coinbase, await registry.ownerOf(tok));
+  
+      // should fail to mint existing token
+      await expect(
+        registry.callStatic.mintSLD(coinbase, 'label_22')
+      ).to.be.revertedWith('ERC721: token already minted');
+      await expect(
+        registry.callStatic.mintSLD(accounts[0], 'label_22')
+      ).to.be.revertedWith('ERC721: token already minted');
+  
+      await registry.burn(tok);
+      await registry.mintSLD(coinbase, 'label_22');
+  
+      assert.equal(coinbase, await registry.ownerOf(tok));
+    })
+
+    it('safe minting SLDs', async () => {
+      const tok = await registry.childIdOf(await registry.root(), 'label_93');
+      await registry.functions['safeMintSLD(address,string)'](coinbase, 'label_93');
+  
+      assert.equal(coinbase, await registry.ownerOf(tok));
+  
+      // should fail to safely mint existing token contract
+      await expect(
+        registry.callStatic['safeMintSLD(address,string)'](coinbase, 'label_93')
+      ).to.be.revertedWith('ERC721: token already minted');
+  
+      await registry.burn(tok)
+  
+      // should fail to safely mint token to non reciever contract
+      await expect(
+        registry.callStatic['safeMintSLD(address,string)'](registry.address, 'label_93')
+      ).to.be.revertedWith('ERC721: transfer to non ERC721Receiver implementer');
+  
+      const simple = await Simple.deploy();
+      await registry.functions['safeMintSLD(address,string)'](simple.address, 'label_93');
+  
+      assert.equal(simple.address, await registry.ownerOf(tok));
+    })
+  });
+
+  describe('Registry (records management)', () => {
     const initializeDomain = async (name) => {
       const tok = await registry.childIdOf(await registry.root(), name);
       await registry.mintSLD(coinbase, name);
