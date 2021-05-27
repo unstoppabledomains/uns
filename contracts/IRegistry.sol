ffe577db01fc1f30f15225308058fde4f6712641
Sergii Bomko <aquila.ua@gmail.com>
2021-05-27 17:13:11 +0400

    remove subdomains

--- contracts/IRegistry.sol
+++ contracts/IRegistry.sol
@@ -27,56 +27,6 @@ interface IRegistry is IERC721MetadataUpgradeable, ISLDMinter, IRecordStorage {
     function isApprovedOrOwner(address spender, uint256 tokenId) external view returns (bool);
 
     /**
-     * @dev Mints a new a child token.
-     * Calculates child token ID using a namehash function.
-     * Requires the msg.sender to be the owner, approved, or operator of tokenId.
-     * Requires the token not exist.
-     * @param to address to receive the ownership of the given token ID
-     * @param tokenId uint256 ID of the parent token
-     * @param label subdomain label of the child token ID
-     */
-    function mintChild(address to, uint256 tokenId, string calldata label) external;
-
-    /**
-     * @dev Transfers the ownership of a child token ID to another address.
-     * Calculates child token ID using a namehash function.
-     * Requires the msg.sender to be the owner, approved, or operator of tokenId.
-     * Requires the token already exist.
-     * @param from current owner of the token
-     * @param to address to receive the ownership of the given token ID
-     * @param tokenId uint256 ID of the token to be transferred
-     * @param label subdomain label of the child token ID
-     */
-    function transferFromChild(address from, address to, uint256 tokenId, string calldata label) external;
-
-    /**
-     * @dev Safely transfers the ownership of a child token ID to another address.
-     * Calculates child token ID using a namehash function.
-     * Implements a ERC721Reciever check unlike transferFromChild.
-     * Requires the msg.sender to be the owner, approved, or operator of tokenId.
-     * Requires the token already exist.
-     * @param from current owner of the token
-     * @param to address to receive the ownership of the given token ID
-     * @param tokenId uint256 parent ID of the token to be transferred
-     * @param label subdomain label of the child token ID
-     * @param _data bytes data to send along with a safe transfer check
-     */
-    function safeTransferFromChild(address from, address to, uint256 tokenId, string calldata label, bytes calldata _data) external;
-
-    /// Shorthand for calling the above ^^^ safeTransferFromChild function with an empty _data parameter. Similar to ERC721.safeTransferFrom.
-    function safeTransferFromChild(address from, address to, uint256 tokenId, string calldata label) external;
-
-    /**
-     * @dev Burns a child token ID.
-     * Calculates child token ID using a namehash function.
-     * Requires the msg.sender to be the owner, approved, or operator of tokenId.
-     * Requires the token already exist.
-     * @param tokenId uint256 ID of the token to be transferred
-     * @param label subdomain label of the child token ID
-     */
-    function burnChild(uint256 tokenId, string calldata label) external;
-
-    /**
      * @dev Gets the resolver of the specified token ID.
      * @param tokenId uint256 ID of the token to query the resolver of
      * @return address currently marked as the resolver of the given token ID
