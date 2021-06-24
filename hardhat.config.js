b3c7fa2f07d6ed06ec12c524b2ef38d1ba109ebb
Sergii Bomko <aquila.ua@gmail.com>
2021-06-24 18:49:13 +0200

    add post-compile task for building artifacts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,12 @@
+const { TASK_COMPILE } = require('hardhat/builtin-tasks/task-names');
+const { task } = require('hardhat/config');
+const path = require('path');
+const fs = require('fs-extra');
+
 /// ENVVAR
 // - ENABLE_GAS_REPORT
 // - ENABLE_CONTRACT_SIZER
 // - CI
-
 const argv = require('yargs/yargs')()
   .env('')
   .boolean('enableGasReport')
@@ -24,6 +28,29 @@ if (argv.enableGasReport) {
 if (argv.enableContractSizer) {
   require('hardhat-contract-sizer');
 }
+
+const cp = async (hre, name, out) => {
+  const _path = path.join(__dirname, hre.config.paths.flatArtifacts, `${out || name}.json`);
+  const artifact = await hre.artifacts.readArtifact(name);
+
+  await fs.outputFile(_path, JSON.stringify(artifact, null, '  '));
+};
+
+task(TASK_COMPILE, 'hook compile task to include post-compile', async function (args, hre, runSuper) {
+  await runSuper();
+  await cp(hre, 'UNSRegistry');
+  await cp(hre, 'Registry', 'CNSRegistry');
+  await cp(hre, 'MintingManager');
+  await cp(hre, 'ProxyReader');
+  await cp(hre, 'SignatureController');
+  await cp(hre, 'MintingController');
+  await cp(hre, 'WhitelistedMinter');
+  await cp(hre, 'URIPrefixController');
+  await cp(hre, 'DomainZoneController');
+  await cp(hre, 'Resolver');
+  await cp(hre, 'TwitterValidationOperator');
+  await cp(hre, 'FreeMinter');
+});
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -53,6 +80,10 @@ module.exports = {
         },
       },
     ],
+  },
+  paths: {
+    artifacts: './.artifacts',
+    flatArtifacts: './artifacts',
   },
   networks: {
     hardhat: {
