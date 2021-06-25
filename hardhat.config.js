78cde3341a9720768e6810fa1b85f715b4e26e79
Sergii Bomko <aquila.ua@gmail.com>
2021-06-25 15:03:55 +0200

    add script for UNS deployment from code

--- hardhat.config.js
+++ hardhat.config.js
@@ -48,6 +48,13 @@ task(TASK_COMPILE, 'hook compile task to perform post-compile task', async (_, h
   }
 });
 
+const settings = {
+  optimizer: {
+    enabled: true,
+    runs: 200,
+  },
+};
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
@@ -56,24 +63,14 @@ module.exports = {
     compilers: [
       {
         version: '0.5.12',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
+        settings,
       },
       {
         version: '0.8.0',
         metadata: {
           bytecodeHash: 'none',
         },
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
+        settings,
       },
     ],
   },
@@ -117,5 +114,28 @@ module.exports = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  uns: {
+    minters: {
+      hardhat: [],
+      localhost: [],
+      rinkeby: [
+        '0xb3B86785A51B950fd54ABdF420ff3B60E091870c',
+        '0x7EF88A779651f26a4967026a32Cae4F01fF8D151',
+        '0x0c71a3494484459bbF9777Dd3109515B2E653CCb',
+        '0x288DA3443BBEBcc7a339182323aa3F23126DFe7a',
+        '0xe45541799119C1D63b60e0F834F3A381D4BEDbea',
+        '0xdb36B5c4cF1D96f020D7629a09cB54ab787414d6',
+        '0x3b9FB7983d897B7fe2fD7563e07e24CbA830b03B',
+        '0x903aA579B9eF13862Fda73275B349017d8fD09eB',
+        '0x7Ac8596cfbb0504DFDEC08d5088B67E7fbfae47f',
+        '0xB83180632b72f988585AF02FC27229bF2Eabd139',
+      ],
+    },
+    linkToken: {
+      hardhat: '',
+      localhost: '',
+      rinkeby: '0x01BE23585060835E02B77ef475b0Cc51aA1e0709',
+    },
   },
 };
