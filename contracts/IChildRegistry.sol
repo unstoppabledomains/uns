cd90df8add9adb8a8c4933910cadd44c762e9177
Sergii Bomko <1164492+aquiladev@users.noreply.github.com>
2021-12-21 22:41:28 +0530

    IChildRegistry is IERC721

--- contracts/IChildRegistry.sol
+++ contracts/IChildRegistry.sol
@@ -3,9 +3,11 @@
 
 pragma solidity ^0.8.0;
 
+import '@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol';
+
 import './@maticnetwork/IChildToken.sol';
 
-interface IChildRegistry is IChildToken {
+interface IChildRegistry is IERC721Upgradeable, IChildToken {
     event WithdrawnBatch(address indexed user, uint256[] tokenIds);
 
     /**
