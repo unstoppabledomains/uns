e3ec748bfc5be751d1b187c24b600157fa63ab6e
Nick Shatilo <shatilo.nick@gmail.com>
2022-04-29 10:47:12 +0100

    Added support for contract-signed metatx into BaseForwarder

--- test/metatx/BaseForwarder.test.js
+++ test/metatx/BaseForwarder.test.js
@@ -7,7 +7,10 @@ const { BigNumber } = ethers;
 
 describe('BaseForwarder', () => {
   let BaseForwarder, forwarder;
-  let signers, owner;
+  let signers, owner, account;
+
+  let ERC1271MockFactory, ERC1271Contract;
+  let ERC1271EmptyMockFactory, ERC1271EmptyMockContract;
 
   const buildExecuteParams = async (selector, params, from, tokenId) => {
     const data = forwarder.interface.encodeFunctionData(selector, params);
@@ -18,28 +21,93 @@ describe('BaseForwarder', () => {
 
   before(async () => {
     signers = await ethers.getSigners();
-    [owner] = signers;
+    [owner, account] = signers;
 
     BaseForwarder = await ethers.getContractFactory('BaseForwarderMock');
+    ERC1271MockFactory = await ethers.getContractFactory('ERC1271Mock');
+    ERC1271EmptyMockFactory = await ethers.getContractFactory('ERC1271EmptyMock');
 
     forwarder = await BaseForwarder.deploy();
+    ERC1271Contract = await ERC1271MockFactory.deploy();
+    ERC1271EmptyMockContract = await ERC1271EmptyMockFactory.deploy();
   });
 
-  it('should revert with native reason', async () => {
-    const tokenId = BigNumber.from(100010);
-    const { req, signature } = await buildExecuteParams('revertWithReason()', [], owner, tokenId);
+  describe('Verify', async () => {
+    it('should verify when the signature is valid', async () => {
+      const tokenId = BigNumber.from(100010);
+      const { req, signature } = await buildExecuteParams('revertWithReason()', [], owner, tokenId);
 
-    await expect(
-      forwarder.execute(req, signature),
-    ).to.be.revertedWith('AAAAAA');
+      expect(await forwarder.verify(req, signature)).to.eq(true);
+    });
+
+    it('should verify that the address matches the recovered one', async () => {
+      const tokenId = BigNumber.from(100010);
+      const { req, signature } = await buildExecuteParams('revertWithReason()', [], owner, tokenId);
+
+      expect(await forwarder.verify({ ...req, from: account.address }, signature)).to.eq(false);
+    });
+
+    it('should verify that nonces match the address once', async () => {
+      const tokenId = BigNumber.from(100010);
+      const { req, signature } = await buildExecuteParams('revertWithReason()', [], owner, tokenId);
+
+      expect(await forwarder.verify({ ...req, nonce: 42 }, signature)).to.eq(false);
+    });
+
+    describe('with metatx from contract addresses', async () => {
+      it('should be able to verify the contract signature', async () => {
+        const tokenId = BigNumber.from(100010);
+        const { req, signature } = await buildExecuteParams('mintAsset()', [], owner, tokenId);
+
+        expect(await forwarder.verify({ ...req, from: ERC1271Contract.address }, signature)).to.eq(true);
+      });
+
+      it('should be able to handle situation when contract does not implement ERC1271', async () => {
+        const tokenId = BigNumber.from(100010);
+        const { req, signature } = await buildExecuteParams('mintAsset()', [], owner, tokenId);
+
+        expect(await forwarder.verify({ ...req, from: ERC1271EmptyMockContract.address }, signature)).to.eq(false);
+      });
+    });
   });
 
-  it('should revert with forwarding reason', async () => {
-    const tokenId = BigNumber.from(100010);
-    const { req, signature } = await buildExecuteParams('revertWithoutReason()', [], owner, tokenId);
+  describe('Execute', async () => {
+    it('should revert with native reason', async () => {
+      const tokenId = BigNumber.from(100010);
+      const { req, signature } = await buildExecuteParams('revertWithReason()', [], owner, tokenId);
 
-    await expect(
-      forwarder.execute(req, signature),
-    ).to.be.revertedWith('BaseForwarder: CALL_FAILED');
+      await expect(
+        forwarder.execute(req, signature),
+      ).to.be.revertedWith('AAAAAA');
+    });
+
+    it('should revert with forwarding reason', async () => {
+      const tokenId = BigNumber.from(100010);
+      const { req, signature } = await buildExecuteParams('revertWithoutReason()', [], owner, tokenId);
+
+      await expect(
+        forwarder.execute(req, signature),
+      ).to.be.revertedWith('BaseForwarder: CALL_FAILED');
+    });
+
+    describe('metatx from contract addresses', async () => {
+      it('should be able to mint asset on behalf of ERC1271Contract', async () => {
+        const tokenId = BigNumber.from(100010);
+        const { req, signature } = await buildExecuteParams('mintAsset()', [], owner, tokenId);
+
+        await forwarder.execute({ ...req, from: ERC1271Contract.address }, signature);
+
+        expect(await forwarder.hasAsset(ERC1271Contract.address)).to.eq(true);
+      });
+
+      it('should revert when contract does not implement ERC1271', async () => {
+        const tokenId = BigNumber.from(100010);
+        const { req, signature } = await buildExecuteParams('mintAsset()', [], owner, tokenId);
+
+        await expect(
+          forwarder.execute({ ...req, from: ERC1271EmptyMockContract.address }, signature),
+        ).to.be.revertedWith('BaseForwarderMock: SIGNATURE_INVALID');
+      });
+    });
   });
 });
