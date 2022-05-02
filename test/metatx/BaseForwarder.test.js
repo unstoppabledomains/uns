115309f859ce64a24b0c2b0433f5a328f32a25b0
Nick Shatilo <shatilo.nick@gmail.com>
2022-05-02 10:42:13 +0100

    Extended ERC1271 mock, added coverage of a complete flow, added smart contract references

--- test/metatx/BaseForwarder.test.js
+++ test/metatx/BaseForwarder.test.js
@@ -9,7 +9,7 @@ describe('BaseForwarder', () => {
   let BaseForwarder, forwarder;
   let signers, owner, account;
 
-  let ERC1271MockFactory, ERC1271Contract;
+  let ERC1271SmartWalletFactory, ERC1271SmartWallet;
   let ERC1271EmptyMockFactory, ERC1271EmptyMockContract;
 
   const buildExecuteParams = async (selector, params, from, tokenId) => {
@@ -24,11 +24,11 @@ describe('BaseForwarder', () => {
     [owner, account] = signers;
 
     BaseForwarder = await ethers.getContractFactory('BaseForwarderMock');
-    ERC1271MockFactory = await ethers.getContractFactory('ERC1271Mock');
+    ERC1271SmartWalletFactory = await ethers.getContractFactory('ERC1271SmartWalletMock');
     ERC1271EmptyMockFactory = await ethers.getContractFactory('ERC1271EmptyMock');
 
     forwarder = await BaseForwarder.deploy();
-    ERC1271Contract = await ERC1271MockFactory.deploy();
+    ERC1271SmartWallet = await ERC1271SmartWalletFactory.deploy(owner.address);
     ERC1271EmptyMockContract = await ERC1271EmptyMockFactory.deploy();
   });
 
@@ -55,11 +55,18 @@ describe('BaseForwarder', () => {
     });
 
     describe('with metatx from contract addresses', async () => {
-      it('should be able to verify the contract signature', async () => {
+      it('should be able to verify the ERC1271 contract signature', async () => {
         const tokenId = BigNumber.from(100010);
         const { req, signature } = await buildExecuteParams('mintAsset()', [], owner, tokenId);
 
-        expect(await forwarder.verify({ ...req, from: ERC1271Contract.address }, signature)).to.eq(true);
+        expect(await forwarder.verify({ ...req, from: ERC1271SmartWallet.address }, signature)).to.eq(true);
+      });
+
+      it('should be able to handle situation when ERC1271 contract returns false', async () => {
+        const tokenId = BigNumber.from(100010);
+        const { req, signature } = await buildExecuteParams('mintAsset()', [], account, tokenId);
+
+        expect(await forwarder.verify({ ...req, from: ERC1271SmartWallet.address }, signature)).to.eq(false);
       });
 
       it('should be able to handle situation when contract does not implement ERC1271', async () => {
@@ -91,13 +98,22 @@ describe('BaseForwarder', () => {
     });
 
     describe('metatx from contract addresses', async () => {
-      it('should be able to mint asset on behalf of ERC1271Contract', async () => {
+      it('should be able to mint asset on behalf of ERC1271SmartWallet', async () => {
         const tokenId = BigNumber.from(100010);
         const { req, signature } = await buildExecuteParams('mintAsset()', [], owner, tokenId);
 
-        await forwarder.execute({ ...req, from: ERC1271Contract.address }, signature);
+        await forwarder.execute({ ...req, from: ERC1271SmartWallet.address }, signature);
 
-        expect(await forwarder.hasAsset(ERC1271Contract.address)).to.eq(true);
+        expect(await forwarder.hasAsset(ERC1271SmartWallet.address)).to.eq(true);
+      });
+
+      it('should revert when the signature is made not on behalf of smart wallet owner', async () => {
+        const tokenId = BigNumber.from(100010);
+        const { req, signature } = await buildExecuteParams('mintAsset()', [], account, tokenId);
+
+        await expect(
+          forwarder.execute({ ...req, from: ERC1271EmptyMockContract.address }, signature),
+        ).to.be.revertedWith('BaseForwarderMock: SIGNATURE_INVALID');
       });
 
       it('should revert when contract does not implement ERC1271', async () => {
