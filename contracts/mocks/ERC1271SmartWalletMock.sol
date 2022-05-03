145a0a81b3271d538a6964c37ee3709040d69b6c
Nick Shatilo <shatilo.nick@gmail.com>
2022-05-03 12:19:26 +0100

    REG-514: updated versions, fixed typos

--- contracts/mocks/ERC1271SmartWalletMock.sol
+++ contracts/mocks/ERC1271SmartWalletMock.sol
@@ -2,7 +2,7 @@ import '@openzeppelin/contracts-upgradeable/interfaces/IERC1271Upgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol';
 
 /* 
-   Smart wallets usually implement ERC1271, which specifies the interace on how the signature of owner (user) should be verified.
+   Smart contract wallets implement ERC1271, which specifies the interface on how the signature of owner (user) should be verified.
    Providing some examples on how is it implemented in different wallets:
    * https://github.com/argentlabs/argent-contracts/blob/ea361609bbda15bf19bac35dbc2af80b720cb27b/contracts/modules/TransactionManager.sol#L232
    * https://github.com/dapperlabs/dapper-contracts/blob/0321efcc80c413dc1d3a3cd30ae02929b3ddadfe/contracts/Wallet/CoreWallet.sol#L360
@@ -19,7 +19,7 @@ contract ERC1271SmartWalletMock is IERC1271Upgradeable {
         _owner = owner;
     }
 
-    function isValidSignature(bytes32 _hash, bytes memory _signature) public view override returns (bytes4 magicValue) {
+    function isValidSignature(bytes32 _hash, bytes memory _signature) public view override returns (bytes4) {
         if(_hash.recover(_signature) == _owner) {
             return _ERC1271_MAGIC_VALUE;
         } else {
