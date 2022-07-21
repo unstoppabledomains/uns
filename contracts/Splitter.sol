e5529ff1c147c19c07bdce3df94c30e680d2c0a3
ivanshapovalov <ivan.shapovalov@unstoppabledomains.com>
2022-07-21 15:49:22 +0300

    REG-582: implement Splitter contract and cover it with tests

---
+++ contracts/Splitter.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.0;
+
+contract Splitter {
+
+    mapping(address => uint256) public balanceMap;
+
+    function split(address recipient, address otherRecipient) external payable {
+        require(msg.value > 0, 'Amount to split should be greater than zero');
+
+        // division by 2.
+        uint256 fairPart = msg.value >> 1;
+        balanceMap[recipient] += fairPart;
+        balanceMap[otherRecipient] += fairPart;
+
+        // check if amount appeared to be odd and if it is - add 1 wei to sender balance for further use.
+        if ((msg.value & 1) == 1) {
+            balanceMap[msg.sender] += 1;
+        }
+    }
+
+    function withdraw() external {
+        uint256 currentBalance = balanceMap[msg.sender];
+        require(currentBalance > 0, 'Zero balance, nothing to withdraw');
+
+        (bool sent,) = msg.sender.call{value: currentBalance}("");
+        require(sent, 'Failed to send Ether');
+        balanceMap[msg.sender] = 0;
+    }
+}
---
+++ test/Splitter.test.js
@@ -0,0 +1,69 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+describe('Splitter', () => {
+  async function countTxFee (tx) {
+    const gasPrice = tx.gasPrice;
+    const gasUsed = (await tx.wait()).gasUsed;
+    return gasPrice * gasUsed;
+  }
+  let Splitter;
+  let splitter;
+  let signers;
+  let sender, receiverOne, receiverTwo, receiverThree, receiverFour;
+
+  before(async () => {
+    signers = await ethers.getSigners();
+    [sender, receiverOne, receiverTwo, receiverThree, receiverFour] = signers;
+
+    Splitter = await ethers.getContractFactory('Splitter');
+    splitter = await Splitter.deploy();
+  });
+
+  it('should revert zero amount split', async () => {
+    await splitter.connect(sender);
+    await expect(splitter.split(receiverOne.address, receiverTwo.address, {
+      value: 0,
+    })).to.be.revertedWith('Amount to split should be greater than zero');
+  });
+
+  it('should revert withdrawal from address with zero balance', async () => {
+    await splitter.connect(receiverFour);
+    await expect(splitter.withdraw()).to.be.revertedWith('Zero balance, nothing to withdraw');
+  });
+
+  it('should equally split amount of wei and leave remaining part on sender balance', async () => {
+    await splitter.connect(sender);
+    await splitter.split(receiverOne.address, receiverTwo.address, {
+      value: 10,
+    });
+
+    expect(await splitter.provider.getBalance(splitter.address)).to.be.equal(10);
+    expect((await splitter.balanceMap(receiverOne.address))).to.be.equal(5);
+    expect((await splitter.balanceMap(receiverTwo.address))).to.be.equal(5);
+    expect((await splitter.balanceMap(sender.address))).to.be.equal(0);
+
+    await splitter.split(receiverOne.address, receiverThree.address, {
+      value: 21,
+    });
+
+    expect(await splitter.provider.getBalance(splitter.address)).to.be.equal(31);
+    expect((await splitter.balanceMap(receiverOne.address))).to.be.equal(15);
+    expect((await splitter.balanceMap(receiverThree.address))).to.be.equal(10);
+    expect((await splitter.balanceMap(sender.address))).to.be.equal(1);
+
+    const beforeWithdrawal = await splitter.provider.getBalance(receiverOne.address);
+    const tx = await splitter.connect(receiverOne).withdraw();
+    const txFee = await countTxFee(tx);
+
+    const afterWithdrawal = await splitter.provider.getBalance(receiverOne.address);
+    const expectedAfterWithdrawal = beforeWithdrawal - txFee + 15;
+
+    // There are difficulties with comparing big numbers in chai, so, counting delta.
+    const actualVsExpectedDelta = expectedAfterWithdrawal - afterWithdrawal;
+    expect(actualVsExpectedDelta).to.be.equal(0);
+
+    expect(await splitter.provider.getBalance(splitter.address)).to.be.equal(16);
+    expect((await splitter.balanceMap(receiverOne.address))).to.be.equal(0);
+  });
+});
