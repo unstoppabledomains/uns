0d0ed1d947e303c371a9665e3eb540ec760d7b8f
Sergii Bomko <1164492+aquiladev@users.noreply.github.com>
2022-10-04 18:11:15 -0500

    add test for proxy

--- test/ReverseLogic.test.js
+++ test/ReverseLogic.test.js
@@ -1,4 +1,4 @@
-const { ethers } = require('hardhat');
+const { ethers, upgrades } = require('hardhat');
 const { expect } = require('chai');
 
 const { TLD, DEAD_ADDRESS } = require('./helpers/constants');
@@ -7,13 +7,13 @@ const { mintDomain } = require('./helpers/registry');
 describe('UNSRegistry - ReverseLogic', () => {
   let UNSRegistry, ReverseLogic;
   let unsRegistry, reverseLogic;
-  let signers, coinbase, owner, reader;
+  let signers, coinbase, owner;
 
   const root = TLD.CRYPTO;
 
   before(async () => {
     signers = await ethers.getSigners();
-    [coinbase, owner, reader] = signers;
+    [coinbase, owner] = signers;
 
     ReverseLogic = await ethers.getContractFactory('ReverseLogic');
     reverseLogic = await ReverseLogic.deploy();
@@ -23,7 +23,10 @@ describe('UNSRegistry - ReverseLogic', () => {
         ReverseLogic: reverseLogic.address,
       },
     });
+  });
 
+  it('should in direct mode', async () => {
+    // arrange
     unsRegistry = await UNSRegistry.deploy();
     await unsRegistry.initialize(coinbase.address);
     await unsRegistry['mint(address,uint256,string)'](
@@ -32,10 +35,7 @@ describe('UNSRegistry - ReverseLogic', () => {
       'crypto',
     );
     await unsRegistry.setTokenURIPrefix('/');
-    await unsRegistry.addProxyReader(reader.address);
-  });
 
-  it('should in direct mode', async () => {
     const tokenId = await mintDomain(
       unsRegistry,
       owner,
@@ -43,6 +43,41 @@ describe('UNSRegistry - ReverseLogic', () => {
       'rr12',
     );
 
+    const _unsRegistry = unsRegistry.connect(owner);
+
+    // act-assert
+    await expect(_unsRegistry.setReverse(tokenId))
+      .to.emit(unsRegistry, 'SetReverse')
+      .withArgs(owner.address, tokenId.toString());
+
+    expect(await unsRegistry.reverseOf(owner.address)).to.be.equal(tokenId);
+
+    await expect(_unsRegistry.removeReverse())
+      .to.emit(unsRegistry, 'RemoveReverse')
+      .withArgs(owner.address);
+
+    expect(await unsRegistry.reverseOf(owner.address)).to.be.equal(0);
+  });
+
+  it('should in proxy mode', async () => {
+    // arrange
+    unsRegistry = await upgrades.deployProxy(UNSRegistry, [coinbase.address], {
+      initializer: 'initialize',
+      unsafeAllowLinkedLibraries: true,
+    });
+    await unsRegistry['mint(address,uint256,string)'](
+      DEAD_ADDRESS,
+      root,
+      'wallet',
+    );
+    await unsRegistry.setTokenURIPrefix('/');
+
+    const tokenId = await mintDomain(
+      unsRegistry,
+      owner,
+      TLD.WALLET,
+      'rr13',
+    );
     const _unsRegistry = unsRegistry.connect(owner);
 
     // act-assert
