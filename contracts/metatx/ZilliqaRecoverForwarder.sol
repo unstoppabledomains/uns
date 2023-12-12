db615c88eb31badb8aaf5868370eed4e03e5478f
Bogdan Gusiev <agresso@gmail.com>
2023-12-12 13:07:25 +0200

    Cleanup

--- contracts/metatx/ZilliqaRecoverForwarder.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-// @author Unstoppable Domains, Inc.
-// @date January 17th, 2023
-
-pragma solidity ^0.8.0;
-
-import './Forwarder.sol';
-
-contract ZilliqaRecoverForwarder is Forwarder {}
