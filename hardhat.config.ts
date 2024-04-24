7edc80906918fba3f78962757094b82669268cd6
Nick Shatilo <nick.shatilo@unstoppabledomains.com>
2024-04-24 12:55:09 +0100

    [REG-1280] Amoy verification on Polygonscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -183,15 +183,15 @@ const config: HardhatUserConfig = {
       mainnet: process.env.ETHERSCAN_API_KEY,
       sepolia: process.env.ETHERSCAN_API_KEY,
       polygon: process.env.POLYGONSCAN_API_KEY,
-      polygonAmoy: process.env.OKLINK_API_KEY,
+      polygonAmoy: process.env.POLYGONSCAN_API_KEY,
     }) as Record<string, string>,
     customChains: [
       {
         network: 'polygonAmoy',
         chainId: 80002,
         urls: {
-          apiURL: 'https://www.oklink.com/api/explorer/v1/contract/verify/async/api/polygonAmoy',
-          browserURL: 'https://www.oklink.com/polygonAmoy',
+          apiURL: 'https://api-amoy.polygonscan.com/api',
+          browserURL: 'https://amoy.polygonscan.com/',
         },
       },
     ],
