dd22c108f1a40b63e85219d15688b06845144af2
Tommy Irish <thomas@unstoppabledomains.com>
2024-05-13 16:00:40 -0400

    add helpful util script for deploying ERC1271SimpleWallet

---
+++ scripts/util/deploy_ERC1271SimpleWallet.ts
@@ -0,0 +1,45 @@
+/**
+ * Pre-req:
+ * - Run: "yarn compile"
+ * - If you want to wrap an address that isn't the deployer, update the `wrappedAddress` below
+ *
+ * Running using a .env file:
+ * 1. Create .env
+ * 2. Add needed vars for desired network(s) -- see hardhat.config.ts for var names
+ * 3. Run via command:
+ * npx dotenv-cli -y -- yarn hardhat run --network <network> scripts/util/deploy_ERC1271SimpleWallet.ts
+ *
+ * Running without using a .env file:
+ * 1. Set env vars or include a the beginning of the command then run:
+ * yarn hardhat run --network <network> scripts/util/deploy_ERC1271SimpleWallet.ts
+ *
+ */
+import { ethers, network } from 'hardhat';
+
+async function main () {
+  console.log('Network:', network.name);
+  const [signer] = await ethers.getSigners();
+
+  // wallet owner (defaults to signer)
+  const wrappedAddress = signer.address;
+
+  const factory = await ethers.getContractFactory('ERC1271SimpleWallet', signer);
+
+  const result = await factory.deploy(
+    wrappedAddress,
+  );
+
+  await result.waitForDeployment();
+
+  console.log('Deployed ERC1271SimpleWallet address:', await result.getAddress());
+  console.log('ERC1271 wrapped address:', wrappedAddress);
+}
+
+
+// eslint-disable-next-line promise/catch-or-return
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+}).finally(() => {
+  process.exit();
+});
