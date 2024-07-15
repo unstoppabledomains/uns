70374143dfbc43abee9f9ab675e48faaab9c5459
derain <derainberk@gmail.com>
2024-07-15 20:04:21 +0300

    Add new TLD's

--- contracts/MintingManager.sol
+++ contracts/MintingManager.sol
@@ -104,7 +104,7 @@ contract MintingManager is ERC2771Context, MinterRole, Blocklist, Pausable, IMin
         __ERC2771Context_init_unchained(forwarder);
         __Pausable_init_unchained();
 
-        string[29] memory tlds = [
+        string[31] memory tlds = [
             'crypto',
             'wallet',
             'x',
@@ -133,7 +133,9 @@ contract MintingManager is ERC2771Context, MinterRole, Blocklist, Pausable, IMin
             'witg',
             'metropolis',
             'wrkx',
-            'secret'
+            'secret',
+            'raiin',
+            'stepn'
         ];
         for (uint256 i = 0; i < tlds.length; i++) {
             _addTld(tlds[i], false);
