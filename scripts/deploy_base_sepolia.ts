5b124500638e197819cdbc8246508b247d275882
roman-gnativ <roman.gnativ@unstoppabledomains.com>
2024-08-01 20:42:56 +0400

    deploy uns to base sepolia

--- scripts/deploy_base_sepolia.ts
+++ scripts/deploy_base_sepolia.ts
@@ -1,8 +1,10 @@
-import { network } from 'hardhat';
+import { ethers, network } from 'hardhat';
 import merge from 'lodash.merge';
 import { getNetworkConfig, mergeNetworkConfig } from '../src/config';
 import { Deployer } from '../src/deployer';
 import { unwrap } from '../src/utils';
+import { deployProxy } from '../src/helpers';
+import { ArtifactName } from '../src/types';
 
 async function main () {
   console.log('Network:', network.name);
@@ -21,10 +23,17 @@ async function main () {
   if (!config) {
     throw new Error(`Config not found for network ${chainId}`);
   }
+  // before running this script, remove only sandbox verification in seaport_proxy_buyer
+  // and check if min_uns_tlds task should be included
 
   const deployer = await Deployer.create();
+  console.log('start uns execute:', config);
   const deployConfig = await deployer.execute(['uns'], config);
+  console.log('start seaport execute', JSON.stringify(deployConfig));
   mergeNetworkConfig(deployConfig);
+
+  const deployConfig2 = await deployer.execute(['seaport_proxy_buyer'], config);
+  mergeNetworkConfig(deployConfig2);
 
   console.log('Deployed!');
 }
